67b9883a3d0e6fb782929e8642b2d8f93d9d9534
David Patrício <david.c.patricio@gmail.com>
2023-07-31 00:24:56 +0100

    Adicionado ficheiro numero_primo.py

---
+++ numero_primo.py
@@ -0,0 +1,21 @@
+import sys
+
+prime = True
+try:
+    num = int(input('Insira um número: '))
+except ValueError:
+    print('Não inseriu um valor numérico.')
+    sys.exit()
+
+if num <= 1:
+    print('Por favor insira um número inteiro igual ou superior a 2.')
+else:
+    for i in range(2, num):
+        if num % i != 0:
+            prime = False
+            break
+
+    if prime:
+        print(f'O número {num} é primo.')
+    else:
+        print(f'O número {num} não é primo.')
