3aff6299e5eb574af3153d40caaeeb85fc8d5a40
David Patrício <david.c.patricio@gmail.com>
2023-09-12 16:53:18 +0100

    numero_primo.py - Corrigir erro (if num % 1 == 0)

--- python/numero_primo.py
+++ python/numero_primo.py
@@ -11,7 +11,7 @@ if num <= 1:
     print('Por favor insira um número inteiro igual ou superior a 2.')
 else:
     for i in range(2, num):
-        if num % i != 0:
+        if num % i == 0:
             prime = False
             break
 
